4a0dc259f792b2eb98567067540f199d34ecd8ea
osparking <jbpark03@gmail.com>
2024-04-20 18:50:47 +0900

    form 및 submit 종점 취급자

---
+++ src/main/java/space/bum/spring_boot/config/ExampleController.java
@@ -0,0 +1,21 @@
+package space.bum.spring_boot.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/example")
+public class ExampleController {
+
+  @GetMapping("/form")
+  public String showForm() {
+    return "csrf-form";
+  }
+
+  @PostMapping("/submit")
+  public String processForm() {
+    return "success";
+  }
+}
