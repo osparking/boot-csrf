0e500cd60d4e399778a90a577da5f03a4a714cb7
osparking <jbpark03@gmail.com>
2024-04-20 18:43:56 +0900

    종속성 2 개

---
+++ src/main/java/space/bum/spring_boot/BootCsrfApplication.java
@@ -0,0 +1,13 @@
+package space.bum.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCsrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCsrfApplication.class, args);
+	}
+
+}
